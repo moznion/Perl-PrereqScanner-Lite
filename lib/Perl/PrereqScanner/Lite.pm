9add6a70c9871bbaab1d4ee4f6714a6b5a29fc15
moznion <moznion@gmail.com>
2014-07-31 17:00:21 +0900

    Checking in changes prior to tagging of version 0.20.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 07ce6ad..9c181b4 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Perl-PrereqScanner-Lite
    
     {{$NEXT}}
    
    +0.20 2014-07-31T07:59:24Z
    +
    +    - Bug fix (#2)
    +    - Upgrade the dependency of Compiler::Lexer
    +
     0.19 2014-07-17T01:37:16Z
    
         - Bug fix (#3)

--- lib/Perl/PrereqScanner/Lite.pm
+++ lib/Perl/PrereqScanner/Lite.pm
@@ -6,7 +6,7 @@ use Compiler::Lexer;
 use CPAN::Meta::Requirements;
 use Perl::PrereqScanner::Lite::Constants;
 
-our $VERSION = "0.19";
+our $VERSION = "0.20";
 
 sub new {
     my ($class, $opt) = @_;
